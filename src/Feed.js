7ba7b5a62dab8d4021bbfc765ade29a7ad0329c2
Jared M <mezquitaj@gmail.com>
2021-12-16 15:56:10 -0500

    started feed's child components

--- src/Feed.js
+++ src/Feed.js
@@ -1,17 +1,99 @@
 import React from 'react'
 import './Feed.css'
+import TweetBox from './TweetBox'
 
 function Feed() {
     return (
         <div className='feed'>
-
-            {/* Header */}
             <div className="feed__header">
                 <h2>Home</h2>
             </div>
 
-            {/* tweet box */}
-
+            <TweetBox/>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
+            <h3>tweet content</h3>
             {/* post */}
             {/* post */}
             {/* post */}
---
+++ src/TweetBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TweetBox.css'
+
+function TweetBox() {
+    return (
+        <div>
+            <h2>tweetBox goes here</h2>
+        </div>
+    )
+}
+
+export default TweetBox
