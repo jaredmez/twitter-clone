dc18a9cb1028cbac2d6149175e414d0bd5a6a7cb
Jared M <mezquitaj@gmail.com>
2021-12-20 19:17:42 -0500

    started working on tweetbox within the main feed component

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import './TweetBox.css'
+import { Avatar, Button } from "@material-ui/core"
 
 function TweetBox() {
     return (
-        <div>
-            <h2>tweetBox goes here</h2>
+        <div className="tweetBox">
+            <form action="">
+                <div className="tweetBox__input">
+									<Avatar></Avatar>
+									<input placeholder="What's happening?" type="text" />
+                </div>
+                <Button className="tweetBox__tweetButton">Tweet</Button>
+            </form>
         </div>
     )
 }
